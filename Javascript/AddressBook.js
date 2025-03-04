5dadf06f65ec164ee1627d168f5a75938eac613b
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-04 10:03:10 +0530

    [Aman] Add.Ability to find existing contact person using their name and edit it.

--- Javascript/AddressBook.js
+++ Javascript/AddressBook.js
@@ -9,17 +9,40 @@ class AddressBook {
         if (contact instanceof Contact) {
             this.contacts.push(contact);
             console.log("Contact added successfully.");
-        }   // Prevent duplicate contacts based on email or phone number
-       else if (this.contacts.some(c => c.email === contact.email || c.phone === contact.phone)) {
+        }else  if (this.contacts.some(c => c.email === contact.email || c.phone === contact.phone)) {
             throw new Error(`Contact with email ${contact.email} or phone ${contact.phone} already exists.`);
         }
-
-        
         else {
             console.log("Invalid contact object.");
         }
     }
 
+    editContact(firstName, lastName, updatedDetails) {
+        let contact = this.contacts.find(
+            c => c.firstName.toLowerCase() === firstName.toLowerCase() &&
+                 c.lastName.toLowerCase() === lastName.toLowerCase()
+        );
+    
+        if (!contact) {
+            console.log(`Contact ${firstName} ${lastName} not found.`);
+            return;
+        }
+    
+        Object.keys(updatedDetails).forEach(key => {
+            if (contact.hasOwnProperty(key) && updatedDetails[key] !== undefined) {
+                contact[key] = updatedDetails[key];
+            }
+        });
+    
+        console.log(`Contact ${firstName} ${lastName} updated successfully.`);
+    }
+    
+    findContactByName(firstName, lastName) {
+        return this.contacts.find(contact => 
+            contact.firstName.toLowerCase() === firstName.toLowerCase() &&
+            contact.lastName.toLowerCase() === lastName.toLowerCase()
+        );
+    }
     displayContacts() {
         if (this.contacts.length === 0) {
             console.log("Address Book is empty.");
